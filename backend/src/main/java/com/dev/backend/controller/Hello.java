c9f86a12ffddc4c05bfc72c3cb1d743a148678e1
Celso <celsodiehl@gmail.com>
2024-11-05 22:12:29 -0300

    Iniciando

---
+++ backend/src/main/java/com/dev/backend/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.dev.backend.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Olá Mundo Spring! "+ new Date();
+    }
+}
+    
